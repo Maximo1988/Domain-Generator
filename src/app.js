8739e4c459ccd5a22b09bdff9a1952e5e661417a
Maximo1988 <max_bd88@hotmail.com>
2022-02-10 01:23:45 +0000

    ultima corrección

--- src/app.js
+++ src/app.js
@@ -6,18 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  console.log("Hello Rigo from the console!");
+  document.getElementById("domain").innerHTML = resultado;
+
   var pronoun = ["the", "our"];
   var adj = ["great", "big"];
   var noun = ["jogger", "racoon"];
   var dominio = [".com", ".edu", ".org"];
 
+  let resultado = "";
   for (let i = 0; i <= pronoun.length - 1; i++) {
     for (let j = 0; j <= adj.length - 1; j++) {
       for (let k = 0; k <= noun.length - 1; k++) {
-        for (let l = 0; l <= dominio.length - 1; l++)
-          console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+        for (let l = 0; l <= dominio.length - 1; l++) {
+          resultado += pronoun[i] + adj[j] + noun[k] + dominio[l] + "</br>";
+          console.log(resultado);
+        }
       }
     }
   }
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("domain").innerHTML = resultado;
 };
